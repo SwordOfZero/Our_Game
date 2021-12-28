58525e02edd6609e57ac7828a87e5fe91768eb68
SwordOfZero <Lev.kindeev@yandex.ru>
2021-12-28 23:41:40 +0300

    start screen ver.1.1

---
+++ Start_screen.py
@@ -0,0 +1,123 @@
+import pygame
+import sys
+import os
+
+music, sound_effects = 1, 1
+screen = 0
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    # если файл не существует, то выходим
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    return image
+
+
+def get_screen(s):
+    global screen
+    screen = s
+
+def take_sound():
+    return music, sound_effects
+
+class StartScreenButtons:
+    def __init__(self):
+        self.options = [199, 445, 199, 241]
+        self.start = [199, 366, 99, 141]
+        self.credits = [199, 430, 299, 341]
+        self.back_button = [49, 171, 499, 541]
+        self.music_button_m = [484, 513, 113, 137]
+        self.music_button_p = [678, 707, 113, 137]
+        self.sound_button_m = [484, 513, 173, 197]
+        self.sound_button_p = [678, 707, 173, 197]
+        self.screen_number = 0
+        self.start_flag = True
+
+    def screens(self, screen_number=0):
+#главный экран
+        if screen_number == 0:
+            image = load_image('start_screen.jpg')
+            screen.blit(image, (0, 0))
+            f1 = pygame.font.Font(None, 75)
+            text1 = f1.render('START', True,
+                              (255, 0, 0))
+            text2 = f1.render('OPTIONS', True,
+                              (255, 0, 0))
+            text3 = f1.render('CREDITS', True,
+                              (255, 0, 0))
+            screen.blit(text1, (200, 100))
+            screen.blit(text2, (200, 200))
+            screen.blit(text3, (200, 300))
+#настройки
+        elif screen_number == 1:
+            image = load_image('options.jpg')
+            screen.blit(image, (0, 0))
+            sound = pygame.font.Font(None, 75)
+            button = pygame.font.Font(None, 75)
+            back_button1 = button.render('Back', True,
+                              (255, 255, 255))
+            screen.blit(back_button1, (50, 500))
+            music_button = sound.render(f'{int(music * 100)}%', True,
+                              (100, 255, 100))
+            sound_button = sound.render(f'{int(sound_effects * 100)}%', True,
+                                        (100, 255, 100))
+            screen.blit(music_button, (530, 95))
+            screen.blit(sound_button, (530, 160))
+#титры
+        elif screen_number == 2:
+            image = load_image('credits.png')
+            screen.blit(image, (0, 0))
+            intro_text = ["Разработчики:",
+                          "  Киндеев Лев, Макаров Макар",
+                          "Художники:",
+                          "  Макаров Макар, Киндеев Лев",
+                          "Отель:",
+                          "  Триваго"]
+            font = pygame.font.Font(None, 30)
+            text_coord = 50
+            for line in intro_text:
+                string_rendered = font.render(line, 1, pygame.Color('white'))
+                intro_rect = string_rendered.get_rect()
+                text_coord += 10
+                intro_rect.top = text_coord
+                intro_rect.x = 10
+                text_coord += intro_rect.height
+                screen.blit(string_rendered, intro_rect)
+
+    def action(self, mouse_pos):
+        global music
+        global sound_effects
+        if self.screen_number == 0: #menu
+            if self.options[0] < mouse_pos[0] < self.options[1] and \
+                    self.options[2] < mouse_pos[1] < self.options[3]:
+                self.screen_number = 1
+            elif self.start[0] < mouse_pos[0] < self.start[1] and \
+                    self.start[2] < mouse_pos[1] < self.start[3]:
+                self.start_flag = False
+            elif self.credits[0] < mouse_pos[0] < self.credits[1] and \
+                    self.credits[2] < mouse_pos[1] < self.credits[3]:
+                self.screen_number = 2
+        elif self.screen_number == 1: #options
+            if self.back_button[0] < mouse_pos[0] < self.back_button[1] and \
+                    self.back_button[2] < mouse_pos[1] < self.back_button[3]:
+                self.screen_number = 0
+            elif self.music_button_m[0] < mouse_pos[0] < self.music_button_m[1] and \
+                    self.music_button_m[2] < mouse_pos[1] < self.music_button_m[3]:
+                if music > 0:
+                    music = (int(music * 100) - 5) / 100
+            elif self.music_button_p[0] < mouse_pos[0] < self.music_button_p[1] and \
+                    self.music_button_p[2] < mouse_pos[1] < self.music_button_p[3]:
+                if music < 1:
+                    music = (int(music * 100) + 5) / 100
+            elif self.sound_button_m[0] < mouse_pos[0] < self.sound_button_m[1] and \
+                    self.sound_button_m[2] < mouse_pos[1] < self.sound_button_m[3]:
+                if sound_effects > 0:
+                    sound_effects = (int(sound_effects * 100) - 5) / 100
+            elif self.sound_button_p[0] < mouse_pos[0] < self.sound_button_p[1] and \
+                    self.sound_button_p[2] < mouse_pos[1] < self.sound_button_p[3]:
+                if sound_effects < 1:
+                    sound_effects = (int(sound_effects * 100) + 5) / 100
+        elif self.screen_number == 2: # credits
+            self.screen_number = 0
--- main.py
+++ main.py
@@ -2,94 +2,27 @@
 import pygame
 import sys
 import os
+import Start_screen
 
 
 pygame.init()
-FPS = 14
+FPS = 24
 size = WIDTH, HEIGHT = 800, 600
 screen = pygame.display.set_mode(size)
-pygame.display.set_caption('Белый круг на чёрном фоне')
+pygame.display.set_caption('EPIC STORY')
 running = True
 clock = pygame.time.Clock()
 pygame.display.flip()
+music, sound_effects = 0, 0
 
-
-class StartScreenButtons:
-    def __init__(self):
-        self.options = [199, 445, 199, 241]
-        self.start = [199, 366, 99, 141]
-        self.credits = [199, 430, 299, 341]
-        self.back_button = [49, 171, 499, 541]
-        self.screen_number = 0
-        self.start_flag = True
-
-    def screens(self, screen_number=0):
-        if screen_number == 0:
-            image = load_image('start_screen.jpg')
-            screen.blit(image, (0, 0))
-            f1 = pygame.font.Font(None, 75)
-            text1 = f1.render('START', True,
-                              (255, 0, 0))
-            text2 = f1.render('OPTIONS', True,
-                              (255, 0, 0))
-            text3 = f1.render('CREDITS', True,
-                              (255, 0, 0))
-            screen.blit(text1, (200, 100))
-            screen.blit(text2, (200, 200))
-            screen.blit(text3, (200, 300))
-        elif screen_number == 1:
-            image = load_image('options.jpg')
-            screen.blit(image, (0, 0))
-            back_button = pygame.font.Font(None, 75)
-            back_button1 = back_button.render('Back', True,
-                              (255, 255, 255))
-            screen.blit(back_button1, (50, 500))
-        elif screen_number == 2:
-            image = load_image('credits.png')
-            screen.blit(image, (0, 0))
-            intro_text = ["Разработчики:",
-                          "  Киндеев Лев, Макаров Макар",
-                          "Художники:",
-                          "  Макаров Макар, Киндеев Лев",
-                          "Отель:",
-                          "  Триваго"]
-            font = pygame.font.Font(None, 30)
-            text_coord = 50
-            for line in intro_text:
-                string_rendered = font.render(line, 1, pygame.Color('white'))
-                intro_rect = string_rendered.get_rect()
-                text_coord += 10
-                intro_rect.top = text_coord
-                intro_rect.x = 10
-                text_coord += intro_rect.height
-                screen.blit(string_rendered, intro_rect)
-
-    def action(self, mouse_pos):
-        if self.screen_number == 0:
-            if self.options[0] < mouse_pos[0] < self.options[1] and \
-                    self.options[2] < mouse_pos[1] < self.options[3]:
-                self.screen_number = 1
-            elif self.start[0] < mouse_pos[0] < self.start[1] and \
-                    self.start[2] < mouse_pos[1] < self.start[3]:
-                self.start_flag = False
-            elif self.credits[0] < mouse_pos[0] < self.credits[1] and \
-                    self.credits[2] < mouse_pos[1] < self.credits[3]:
-                self.screen_number = 2
-        elif self.screen_number == 1:
-            if self.back_button[0] < mouse_pos[0] < self.back_button[1] and \
-                    self.back_button[2] < mouse_pos[1] < self.back_button[3]:
-                self.screen_number = 0
-        elif self.screen_number == 2:
-            self.screen_number = 0
-
-
-
+pygame.mixer.init()
+pygame.mixer.music.load('data/this_silence_is_mine.mp3')
+pygame.mixer.music.play()
 
 
 def mouse(pos):
     image = load_image("mouse.png")
     screen.blit(image, pos)
-
 
 
 def load_image(name, colorkey=None):
@@ -108,10 +41,15 @@ def terminate():
 
 
 def start_screen():
-    s = StartScreenButtons()
+    global sound_effects
+    global music
+    Start_screen.get_screen(screen)
+    s = Start_screen.StartScreenButtons()
     x, y = (-70, -70)
     mouse_cursor = False
     while s.start_flag:
+        music, sound_effects = Start_screen.take_sound()
+        pygame.mixer.music.set_volume(music)
         s.screens(s.screen_number)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -143,4 +81,3 @@ while running:
     clock.tick(FPS)
     screen.fill((0, 0, 0))
 pygame.quit()
-
